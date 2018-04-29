758cc73e3382a69a6718eeda31832cb6213985f0
Neargye <neargye@gmail.com>
2018-04-30 04:36:10 +0500

    using ::std

--- include/semver.hpp
+++ include/semver.hpp
@@ -55,27 +55,27 @@ namespace semver {
 
 #pragma pack(push, 1)
 struct Version final {
-  static constexpr const std::size_t kVersionStringLength = 22;
+  static constexpr const ::std::size_t kVersionStringLength = 22;
   // 3(<major>) + 1(.) + 3(<minor>) + 1(.) + 3(<patch>) +
   // 1(-) + 5(<prerelease>) + 1(.) + 3(<prereleaseversion>) + 1('\0') = 22
-  enum class PreReleaseType : std::int8_t {
+  enum class PreReleaseType : ::std::int8_t {
     kAlpha = 0,
     kBetha = 1,
     kReleaseCandidate = 2,
     kNone = 3,
   };
 
-  std::uint8_t major;
-  std::uint8_t minor;
-  std::uint8_t patch;
+  ::uint8_t major;
+  ::std::uint8_t minor;
+  ::std::uint8_t patch;
   PreReleaseType pre_release_type;
-  std::uint8_t pre_release_version;
+  ::std::uint8_t pre_release_version;
 
-  constexpr Version(const std::uint8_t,
-                    const std::uint8_t,
-                    const std::uint8_t,
+  constexpr Version(const ::std::uint8_t,
+                    const ::std::uint8_t,
+                    const ::std::uint8_t,
                     const PreReleaseType = PreReleaseType::kNone,
-                    const std::uint8_t = static_cast<std::uint8_t>(0)) noexcept;
+                    const ::std::uint8_t = static_cast<::std::uint8_t>(0)) noexcept;
 
   constexpr Version() noexcept;
 
@@ -83,7 +83,7 @@ struct Version final {
 
   constexpr Version(Version&&) noexcept = default;
 
-  explicit Version(const std::string&) noexcept;
+  explicit Version(const ::std::string&) noexcept;
 
   explicit Version(const char*) noexcept;
 
@@ -95,23 +95,23 @@ struct Version final {
 
   constexpr bool IsValid() const noexcept;
 
-  std::size_t ToString(char*, const std::size_t = kVersionStringLength) const noexcept;
+  ::std::size_t ToString(char*, const ::std::size_t = kVersionStringLength) const noexcept;
 
-  std::string ToString() const noexcept;
+  ::std::string ToString() const noexcept;
 
   bool FromString(const char*) noexcept;
 
-  bool FromString(const std::string&) noexcept;
+  bool FromString(const ::std::string&) noexcept;
 };
 #pragma pack(pop)
 
-std::size_t ToString(const Version&, char*, const std::size_t = Version::kVersionStringLength) noexcept;
+::std::size_t ToString(const Version&, char*, const ::std::size_t = Version::kVersionStringLength) noexcept;
 
-std::string ToString(const Version&) noexcept;
+::std::string ToString(const Version&) noexcept;
 
 Version FromString(const char*) noexcept;
 
-Version FromString(const std::string&) noexcept;
+Version FromString(const ::std::string&) noexcept;
 
 constexpr bool operator==(const Version&, const Version&) noexcept;
 
@@ -125,11 +125,11 @@ constexpr bool operator<(const Version&, const Version&) noexcept;
 
 constexpr bool operator<=(const Version&, const Version&) noexcept;
 
-std::ostream& operator<<(std::ostream&, const Version&) noexcept;
+::std::ostream& operator<<(::std::ostream&, const Version&) noexcept;
 
-std::istream& operator>>(std::istream&, Version&) noexcept;
+::std::istream& operator>>(::std::istream&, Version&) noexcept;
 
-Version operator"" _version(const char*, const std::size_t) noexcept;
+Version operator"" _version(const char*, const ::std::size_t) noexcept;
 
 namespace detail {
 
@@ -183,24 +183,24 @@ inline constexpr bool IsGreater(const Version& v1, const Version& v2) noexcept {
 
 } // namespace detail
 
-inline constexpr Version::Version(const std::uint8_t major,
-                                  const std::uint8_t minor,
-                                  const std::uint8_t patch,
+inline constexpr Version::Version(const ::std::uint8_t major,
+                                  const ::std::uint8_t minor,
+                                  const ::std::uint8_t patch,
                                   const PreReleaseType pre_release_type,
-                                  const std::uint8_t pre_release_version) noexcept
+                                  const ::std::uint8_t pre_release_version) noexcept
     : major{major},
       minor{minor},
       patch{patch},
       pre_release_type{pre_release_type},
       pre_release_version{(pre_release_type == PreReleaseType::kNone)
-                              ? static_cast<std::uint8_t>(0)
+                              ? static_cast<::std::uint8_t>(0)
                               : pre_release_version} {}
 
 inline constexpr Version::Version() noexcept : Version(0, 1, 0)  {
   // https://semver.org/#how-should-i-deal-with-revisions-in-the-0yz-initial-development-phase
 }
 
-inline Version::Version(const std::string& s) noexcept : Version(0, 0, 0)  {
+inline Version::Version(const ::std::string& s) noexcept : Version(0, 0, 0)  {
   (*this) = semver::FromString(s);
 }
 
@@ -212,11 +212,11 @@ inline constexpr bool Version::IsValid() const noexcept {
   return ((pre_release_type >= PreReleaseType::kAlpha) && (pre_release_type <= PreReleaseType::kNone));
 }
 
-inline std::size_t Version::ToString(char* s, const std::size_t length) const noexcept {
+inline ::std::size_t Version::ToString(char* s, const ::std::size_t length) const noexcept {
   return semver::ToString(*this, s, length);
 }
 
-inline std::string Version::ToString() const noexcept {
+inline ::std::string Version::ToString() const noexcept {
   return semver::ToString(*this);
 }
 
@@ -224,7 +224,7 @@ inline bool Version::FromString(const char* s) noexcept {
   return ((*this) = semver::FromString(s)).IsValid();
 }
 
-inline bool Version::FromString(const std::string& s) noexcept {
+inline bool Version::FromString(const ::std::string& s) noexcept {
   return ((*this) = semver::FromString(s)).IsValid();
 }
 
@@ -252,21 +252,21 @@ inline constexpr bool operator<=(const Version& v1, const Version& v2) noexcept 
   return ((v1.IsValid() && v2.IsValid()) && (detail::IsEquals(v1, v2) || detail::IsLess(v1, v2)));
 }
 
-inline std::ostream& operator<<(std::ostream& os, const Version& v) noexcept {
-  std::array<char, Version::kVersionStringLength> version = {{'\0'}};
+inline ::std::ostream& operator<<(::std::ostream& os, const Version& v) noexcept {
+  ::std::array<char, Version::kVersionStringLength> version = {{'\0'}};
   v.ToString(version.data());
   os << version.data();
   return os;
 }
 
-inline std::istream& operator>>(std::istream& is, Version& v) noexcept {
-  std::array<char, Version::kVersionStringLength> version = {{'\0'}};
+inline ::std::istream& operator>>(::std::istream& is, Version& v) noexcept {
+  ::std::array<char, Version::kVersionStringLength> version = {{'\0'}};
   is >> version.data();
   v.FromString(version.data());
   return is;
 }
 
-inline std::size_t ToString(const Version& v, char* s, const std::size_t length) noexcept {
+inline ::std::size_t ToString(const Version& v, char* s, const ::std::size_t length) noexcept {
   if (s == nullptr || !v.IsValid()) {
     return 0;
   }
@@ -276,11 +276,11 @@ inline std::size_t ToString(const Version& v, char* s, const std::size_t length)
   switch (v.pre_release_type) {
     case Version::PreReleaseType::kAlpha: {
       if (v.pre_release_version == 0) {
-        size = std::snprintf(s, length,
+        size = ::std::snprintf(s, length,
                              "%" PRIu8 ".%" PRIu8 ".%" PRIu8 "-alpha",
                              v.major, v.minor, v.patch);
       } else {
-        size = std::snprintf(s, length,
+        size = ::std::snprintf(s, length,
                              "%" PRIu8 ".%" PRIu8 ".%" PRIu8 "-alpha.%" PRIu8,
                              v.major, v.minor, v.patch, v.pre_release_version);
       }
@@ -288,11 +288,11 @@ inline std::size_t ToString(const Version& v, char* s, const std::size_t length)
     }
     case Version::PreReleaseType::kBetha: {
       if (v.pre_release_version == 0) {
-        size = std::snprintf(s, length,
+        size = ::std::snprintf(s, length,
                              "%" PRIu8 ".%" PRIu8 ".%" PRIu8 "-betha",
                              v.major, v.minor, v.patch);
       } else {
-        size = std::snprintf(s, length,
+        size = ::std::snprintf(s, length,
                              "%" PRIu8 ".%" PRIu8 ".%" PRIu8 "-betha.%" PRIu8,
                              v.major, v.minor, v.patch, v.pre_release_version);
       }
@@ -300,18 +300,18 @@ inline std::size_t ToString(const Version& v, char* s, const std::size_t length)
     }
     case Version::PreReleaseType::kReleaseCandidate: {
       if (v.pre_release_version == 0) {
-        size = std::snprintf(s, length,
+        size = ::std::snprintf(s, length,
                              "%" PRIu8 ".%" PRIu8 ".%" PRIu8 "-rc",
                              v.major, v.minor, v.patch);
       } else {
-        size = std::snprintf(s, length,
+        size = ::std::snprintf(s, length,
                              "%" PRIu8 ".%" PRIu8 ".%" PRIu8 "-rc.%" PRIu8,
                              v.major, v.minor, v.patch, v.pre_release_version);
       }
       break;
     }
     case Version::PreReleaseType::kNone: {
-      size = std::snprintf(s, length,
+      size = ::std::snprintf(s, length,
                            "%" PRIu8 ".%" PRIu8 ".%" PRIu8,
                            v.major, v.minor, v.patch);
       break;
@@ -321,22 +321,22 @@ inline std::size_t ToString(const Version& v, char* s, const std::size_t length)
   }
 
   if (size > 0) {
-    return static_cast<std::size_t>(size);
+    return static_cast<::std::size_t>(size);
   }
 
   return 0;
 }
 
-inline std::string ToString(const Version& v) noexcept {
-  std::string s(Version::kVersionStringLength, '\0');
-  const std::size_t size = ToString(v, &s[0], s.length());
+inline ::std::string ToString(const Version& v) noexcept {
+  ::std::string s(Version::kVersionStringLength, '\0');
+  const ::std::size_t size = ToString(v, &s[0], s.length());
   if (size > 0) {
     s.resize(size);
     s.shrink_to_fit();
     return s;
   }
 
-  return std::string{};
+  return ::std::string{};
 }
 
 inline Version FromString(const char* s) noexcept {
@@ -345,33 +345,33 @@ inline Version FromString(const char* s) noexcept {
     return v;
   }
 
-  std::array<char, 7> prerelease = {{'\0'}}; // 5(<prerelease>) + 1(.) + 1('\0') = 7
-  const int num = std::sscanf(s, "%" SCNu8 ".%" SCNu8 ".%" SCNu8 "-%[^0-9]%" SCNu8,
+  ::std::array<char, 7> prerelease = {{'\0'}}; // 5(<prerelease>) + 1(.) + 1('\0') = 7
+  const int num = ::std::sscanf(s, "%" SCNu8 ".%" SCNu8 ".%" SCNu8 "-%[^0-9]%" SCNu8,
                               &(v.major), &(v.minor), &(v.patch),
                               prerelease.data(), &(v.pre_release_version));
 
   if (num >= 3 && num <= 5) {
-    if (std::strncmp(prerelease.data(), "alpha.", 6) == 0) {
+    if (::std::strncmp(prerelease.data(), "alpha.", 6) == 0) {
       v.pre_release_type = Version::PreReleaseType::kAlpha;
-    } else if (std::strncmp(prerelease.data(), "alpha", 5) == 0) {
+    } else if (::std::strncmp(prerelease.data(), "alpha", 5) == 0) {
       v.pre_release_type = Version::PreReleaseType::kAlpha;
       v.pre_release_version = 0;
-    } else if (std::strncmp(prerelease.data(), "betha.", 6) == 0) {
+    } else if (::std::strncmp(prerelease.data(), "betha.", 6) == 0) {
       v.pre_release_type = Version::PreReleaseType::kBetha;
-    } else if (std::strncmp(prerelease.data(), "betha", 5) == 0) {
+    } else if (::std::strncmp(prerelease.data(), "betha", 5) == 0) {
       v.pre_release_type = Version::PreReleaseType::kBetha;
       v.pre_release_version = 0;
-    } else if (std::strncmp(prerelease.data(), "rc.", 3) == 0) {
+    } else if (::std::strncmp(prerelease.data(), "rc.", 3) == 0) {
       v.pre_release_type = Version::PreReleaseType::kReleaseCandidate;
-    } else if (std::strncmp(prerelease.data(), "rc", 2) == 0) {
+    } else if (::std::strncmp(prerelease.data(), "rc", 2) == 0) {
       v.pre_release_type = Version::PreReleaseType::kReleaseCandidate;
       v.pre_release_version = 0;
-    } else if (std::strncmp(prerelease.data(), "\0\0\0\0\0\0\0", 7) == 0) {
+    } else if (::std::strncmp(prerelease.data(), "\0\0\0\0\0\0\0", 7) == 0) {
       v.pre_release_type = Version::PreReleaseType::kNone;
       v.pre_release_version = 0;
     }
 
-    if(std::strcmp(v.ToString().c_str(), s) != 0) {
+    if(::std::strcmp(v.ToString().c_str(), s) != 0) {
       v.pre_release_type = static_cast<Version::PreReleaseType>(-1);
     }
   }
@@ -379,11 +379,11 @@ inline Version FromString(const char* s) noexcept {
   return v;
 }
 
-inline Version FromString(const std::string& s) noexcept {
+inline Version FromString(const ::std::string& s) noexcept {
   return FromString(s.c_str());
 }
 
-inline Version operator"" _version(const char* s, const std::size_t) noexcept {
+inline Version operator"" _version(const char* s, const ::std::size_t) noexcept {
   return FromString(s);
 }
 
