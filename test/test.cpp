344ed6d183ee8683653704fb64615e3b288805ae
neargye <neargye@gmail.com>
2019-07-09 16:41:35 +0500

    fix clang build

--- test/test.cpp
+++ test/test.cpp
@@ -130,7 +130,7 @@ TEST_CASE("constructors") {
 }
 
 TEST_CASE("operators") {
-  constexpr std::array<version, 56> versions = {
+  constexpr std::array<version, 56> versions = {{
       version{0, 0, 0, prerelease::alpha, 0},
       version{0, 0, 0, prerelease::alpha, 1},
       version{0, 0, 0, prerelease::beta, 0},
@@ -194,7 +194,7 @@ TEST_CASE("operators") {
       version{1, 1, 1, prerelease::rc, 0},
       version{1, 1, 1, prerelease::rc, 1},
       version{1, 1, 1},
-  };
+  }};
 
   SECTION("operator =") {
     constexpr version v1{1, 2, 3, prerelease::rc, 4};
@@ -288,7 +288,7 @@ TEST_CASE("operators") {
 }
 
 TEST_CASE("from/to string") {
-  constexpr std::array<version, 19> versions = {
+  constexpr std::array<version, 19> versions = {{
       version{1, 2, 3},
       version{255, 255, 255},
       version{0, 0, 0},
@@ -312,9 +312,9 @@ TEST_CASE("from/to string") {
       version{1, 2, 3, prerelease::rc, 4},
       version{255, 255, 255, prerelease::rc, 255},
       version{0, 0, 0, prerelease::rc, 0},
-  };
+  }};
 
-  constexpr std::array<std::string_view, 19> versions_strings = {
+  constexpr std::array<std::string_view, 19> versions_strings = {{
       "1.2.3",
       "255.255.255",
       "0.0.0",
@@ -338,7 +338,7 @@ TEST_CASE("from/to string") {
       "1.2.3-rc.4",
       "255.255.255-rc.255",
       "0.0.0-rc",
-  };
+  }};
 
   SECTION("from string") {
     version v;
