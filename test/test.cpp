fccd40940d0233d78b1a69acdd92afb3be5b9c70
Naratzul <naratzul@gmail.com>
2020-09-20 12:57:36 +0500

    add tests for ranges with prerelease tags

--- test/test.cpp
+++ test/test.cpp
@@ -461,3 +461,30 @@ TEST_CASE("ranges") {
     STATIC_REQUIRE_FALSE(range.contains(v5));
   }
 }
+
+TEST_CASE("ranges with prerelase tags") {
+  SECTION("prerelease tags") {
+    constexpr range r1{">1.2.3-alpha.3"};
+    constexpr version v1{"1.2.3-alpha.7"};
+    constexpr version v2{"3.4.5-alpha.9"};
+    constexpr version v3{"3.4.5"};
+    constexpr version v4{"1.2.3-alpha.3"};
+
+    STATIC_REQUIRE(r1.contains(v1));
+    STATIC_REQUIRE_FALSE(r1.contains(v2));
+    STATIC_REQUIRE(r1.contains(v3));
+    STATIC_REQUIRE(r1.contains(v4));
+  }
+
+  SECTION("prelease type comparison") {
+    constexpr version v1{"1.0.0-alpha.123"};
+    constexpr version v2{"1.0.0-beta.123"};
+    constexpr version v3{"1.0.0-rc.123"};
+
+    constexpr range r1{"<1.2.3-alpha.124"};
+    constexpr range r2{"<1.0.0-beta.123"};
+    constexpr range r3{"<1.0.0-rc.124"};
+
+    
+  }
+}
