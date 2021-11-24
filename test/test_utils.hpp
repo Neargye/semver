2ec179dfefcd8d5f7ed8a1db6a40a42362dd12aa
Naratzul <naratzul@gmail.com>
2021-11-24 23:41:21 +0500

    fix tabs

--- test/test_utils.hpp
+++ test/test_utils.hpp
@@ -3,21 +3,21 @@
 #include <catch.hpp>
 
 #define STATIC_CHECK_OP_AND_REVERSE(v1, op, v2) \
-static_assert(v1 op v2);                      \
-static_assert(v2 op v1);
+  static_assert(v1 op v2);                      \
+  static_assert(v2 op v1);
 
 #define STATIC_CHECK_OP_AND_REVERSE_FALSE(v1, op, v2) \
-static_assert(v1 op v2);                            \
-static_assert(!(v2 op v1));
+  static_assert(v1 op v2);                            \
+  static_assert(!(v2 op v1));
 
 #define CHECK_OP_AND_REVERSE(v1, op, v2) \
-REQUIRE(v1 op v2);                     \
-REQUIRE(v2 op v1);
+  REQUIRE(v1 op v2);                     \
+  REQUIRE(v2 op v1);
 
 #define CHECK_OP_AND_REVERSE_FALSE(v1, op, v2) \
-REQUIRE(v1 op v2);                           \
-REQUIRE_FALSE(v2 op v1);
+  REQUIRE(v1 op v2);                           \
+  REQUIRE_FALSE(v2 op v1);
 
 #define CHECK_FALSE_OP_AND_REVERSE(v1, op, v2) \
-REQUIRE_FALSE(v1 op v2);                     \
-REQUIRE_FALSE(v2 op v1);
+  REQUIRE_FALSE(v1 op v2);                     \
+  REQUIRE_FALSE(v2 op v1);
