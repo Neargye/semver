e10d9ec226bf25068b2739ef54449f804af0a438
naratzul <naratzul@gmail.com>
2022-02-06 23:48:39 +0500

    fix macro

--- test/test_utils.hpp
+++ test/test_utils.hpp
@@ -3,21 +3,21 @@
 #include <catch.hpp>
 
 #define STATIC_CHECK_OP_AND_REVERSE(v1, op, v2) \
-  static_assert(op(v1, v2));                      \
+  static_assert(op(v1, v2));                    \
   static_assert(op(v2, v1));
 
 #define STATIC_CHECK_OP_AND_REVERSE_FALSE(v1, op, v2) \
-  static_assert(op(v1, v2));                            \
+  static_assert(op(v1, v2));                          \
   static_assert(!op(v2, v1));
 
 #define CHECK_OP_AND_REVERSE(v1, op, v2) \
-  REQUIRE(op(v1, v2));                     \
+  REQUIRE(op(v1, v2));                   \
   REQUIRE(op(v2, v1));
 
 #define CHECK_OP_AND_REVERSE_FALSE(v1, op, v2) \
-  REQUIRE(op(v1, v2));                           \
+  REQUIRE(op(v1, v2));                         \
   REQUIRE_FALSE(op(v2, v1));
 
 #define CHECK_FALSE_OP_AND_REVERSE(v1, op, v2) \
-  REQUIRE_FALSE(op(v1, v2));                     \
+  REQUIRE_FALSE(op(v1, v2));                   \
   REQUIRE_FALSE(op(v2, v1));
