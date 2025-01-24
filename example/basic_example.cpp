1aeb3bea5c17d0e62d127980b546c2a8a1e131e8
neargye <neargye@gmail.com>
2025-01-24 17:32:50 +0400

    fix c

--- example/basic_example.cpp
+++ example/basic_example.cpp
@@ -21,7 +21,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE  OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-#include <сassert>
+#include <cassert>
 #include <iostream>
 #include "semver.hpp"
 
